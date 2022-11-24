fede7827017b9b1de0e7893980d5015c0f6ec354
CrisConSer <cristinacondeserrano@gmail.com>
2022-11-24 18:05:12 +0100

    Tema04EjercicioEstaciones

---
+++ Ejercicios Sentencia (If y Switch)/Tema04EjercicioEstaciones.java
@@ -0,0 +1,19 @@
+package Ejercicios_Sentencia_(If_y_Switch);
+
+/*Tema04EjercicioEstaciones.java
+* Programa que ordena tres números enteros introducidos por teclado
+* y se orden de menor a mayor
+* @CConde
+*/
+
+public class Tema04EjercicioEstaciones {
+    public static void main(String[] args) {
+    
+    System.out.print("Por favor, introduzca un numero de estacion: ");
+    Scanner s= new Scanner (System.in);
+    
+    String nombreEstacion;
+
+    switch (estacion) {
+        case 1:
+  
