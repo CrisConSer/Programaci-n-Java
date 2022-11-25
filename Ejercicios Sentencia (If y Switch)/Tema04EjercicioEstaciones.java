8d3360bcaf1f87d3bdb2419200ba2da28a586756
CrisConSer <cristinacondeserrano@gmail.com>
2022-11-25 20:52:09 +0100

    Corrección

--- Ejercicios Sentencia (If y Switch)/Tema04EjercicioEstaciones.java
+++ Ejercicios Sentencia (If y Switch)/Tema04EjercicioEstaciones.java
@@ -1,4 +1,4 @@
-package Ejercicios_Sentencia_(If_y_Switch);
+import java.util.Scanner;
 
 /*Tema04EjercicioEstaciones.java
 * Programa que ordena tres números enteros introducidos por teclado
@@ -8,12 +8,44 @@ package Ejercicios_Sentencia_(If_y_Switch);
 
 public class Tema04EjercicioEstaciones {
     public static void main(String[] args) {
-    
-    System.out.print("Por favor, introduzca un numero de estacion: ");
-    Scanner s= new Scanner (System.in);
-    
-    String nombreEstacion;
+        Scanner s= new Scanner (System.in);
 
-    switch (estacion) {
+        System.out.print("Por favor, introduzca un numero de mes: ");
+        int numeroDelMes = s.nextInt();
+        String estacion;
+
+        switch (numeroDelMes) {
         case 1:
-  
+        case 2:
+        case 3:
+            estacion = "invierno";
+        break;
+
+        case 4:
+        case 5:
+        case 6:
+            estacion = "primavera";
+        break;
+        
+        case 7:
+        case 8:
+        case 9:
+            estacion = "verano";
+        break;
+        
+        case 10:
+        case 11:
+        case 12:
+            estacion = "otoño";
+        break;
+        
+
+        default:
+                estacion = "no existe ese mes";
+        }
+                
+        System.out.println("La estacion correspondiente al mes elegido es " + estacion);
+        
+    }
+}
+
